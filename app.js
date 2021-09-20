c7dbe34d4515d6e0441558beae910bd8abf47524
Руслан Коноваленко <konovalenko.ruslan@es.lan>
2021-09-20 18:07:06 +0300

    remove body-parser

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
-const bodyParser = require('body-parser')
 
 const config = require('./config/config')
 
@@ -25,8 +24,8 @@ require('./models/User')
 app.use(passport.initialize())
 require('./middleware/passport')(passport)
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.options('*', cors())
 app.use(cors({ optionsSuccessStatus: 200, allowedHeaders: true }))
